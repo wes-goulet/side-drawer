22a3c5982dbdb6e0d71009dcd5280b36528c6c52
Wes Peter <wes@wespeter.com>
2019-01-16 22:47:33 -0800

    using shadow dom

--- lib/side-drawer.ts
+++ lib/side-drawer.ts
@@ -1,5 +1,27 @@
-import "./style.css";
+export default class SideDrawer extends HTMLElement {
+  constructor() {
+    super();
 
-export default class SideDrawer extends HTMLElement {}
+    this.attachShadow({ mode: "open" });
+    // @ts-ignore
+    this.shadowRoot.innerHTML = html`
+      <style>
+        .container {
+          height: 100%;
+          width: 0;
+          position: fixed;
+          z-index: 1;
+          top: 0;
+          left: 0;
+          overflow-x: hidden;
+          transition: 0.5s;
+        }
+      </style>
+
+      <div class="container"><slot></slot></div>
+      >
+    `;
+  }
+}
 
 customElements.define("side-drawer", SideDrawer);
