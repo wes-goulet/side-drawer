8a7043992dcc6db741d94395669ff8c17f065c80
Wes Peter <wes@wespeter.com>
2019-01-21 09:07:26 -0800

    add overlay that closes drawer on click

--- lib/side-drawer.ts
+++ lib/side-drawer.ts
@@ -10,12 +10,55 @@ const tmpl = document.createElement("template");
 tmpl.innerHTML = `<style>${style}</style>${template}`;
 
 export class SideDrawer extends HTMLElement {
+  private _freeSpaceDiv: HTMLElement | null;
+
   constructor() {
     super();
 
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    this._freeSpaceDiv = shadowRoot.getElementById("fs");
   }
+
+  connectedCallback() {
+    if (this._freeSpaceDiv) {
+      this._freeSpaceDiv.addEventListener(
+        "click",
+        this.handleFreeSpaceDivClick
+      );
+    }
+  }
+
+  get open() {
+    return this.hasAttribute("open");
+  }
+
+  set open(isOpen) {
+    if (isOpen) {
+      this.setAttribute("open", "");
+    } else {
+      this.removeAttribute("open");
+    }
+  }
+
+  // static get observedAttributes() {
+  //   return ["open"];
+  // }
+
+  // attributeChangedCallback(_name: string, _oldValue: any, _newValue: any) {
+  //   // When the drawer is closed, update keyboard/screen reader behavior.
+  //   if (!this.open) {
+  //     this.setAttribute("tabindex", "-1");
+  //     this.setAttribute("aria-disabled", "true");
+  //   } else {
+  //     this.setAttribute("tabindex", "0");
+  //     this.setAttribute("aria-disabled", "false");
+  //   }
+  // }
+
+  private handleFreeSpaceDivClick = (_e: any) => {
+    this.open = false;
+  };
 }
 
 customElements.define("side-drawer", SideDrawer);
